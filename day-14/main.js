8b0a455e9a0063a1620294b4b7bdd4f052c80c19
Victoriya Moiseeva <moiseevaviktoriya@gmail.com>
2025-01-25 00:00:01 +0300

    day-14

--- day-14/main.js
+++ day-14/main.js
@@ -1,1 +1,10 @@
 import "./styles.css";
+const rangeInput = document.querySelector('#range-input');
+const rootElement = document.documentElement;
+
+rangeInput.value = getComputedStyle(document.body).getPropertyValue("--base-hue");
+
+rangeInput.addEventListener('input', (event) => {
+    rangeInput.value = event.target.value;
+    rootElement.style.setProperty("--base-hue", event.target.value);
+})
